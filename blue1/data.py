71a3c596168489eb0960f33eb140499802032530
Evan Overman <evanrileyoverman@gmail.com>
2023-12-03 13:34:51 -0800

    data for working with sheets

---
+++ blue1/data.py
@@ -0,0 +1,7 @@
+# Copyright (c) Evan Overman 2023 (https://an-prata.it/)
+# Licensed under the MIT License
+# See LICENSE file at repository root for details.
+
+TEAM_FIELD = 'Team Number'
+MATCH_FIELD = 'Match Number'
+
