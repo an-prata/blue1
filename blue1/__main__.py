ff80f2cb2f1744ced595e6b531753e38fdd6225d
Evan Overman <evanrileyoverman@gmail.com>
2023-12-03 20:27:15 -0800

    save logging in file

--- blue1/__main__.py
+++ blue1/__main__.py
@@ -5,8 +5,10 @@
 import asyncio
 from . import bot
 from . import tba
+from . import logging
 
 if __name__ == '__main__':
+    logging.open_log_file()
     t = tba.tba_from_env()
     b = bot.blue1_from_env(t)
     asyncio.get_event_loop().run_until_complete(b.start())
--- blue1/logging.py
+++ blue1/logging.py
@@ -5,6 +5,10 @@
 import sys
 import datetime
 from enum import StrEnum
+from . import state
+
+LOG_PATH = f"{state.XDG_STATE_HOME}/blue1.log"
+LOG_FILE = None
 
 class Color(StrEnum):
     RED    = "\033[31m"
@@ -12,6 +16,23 @@ class Color(StrEnum):
     BLUE   = "\033[34m"
     BOLD   = "\033[1m"
     NORMAL = "\033[0m"
+
+
+def open_log_file(path: str=LOG_PATH) -> bool:
+    """
+    Opens the given file path, or a default file path, to save logs to.
+    """
+
+    global LOG_FILE
+
+    try:
+        LOG_FILE = open(path, 'a')
+        log("LOG", f"Opened '{path}' for logging")
+        return True
+    except OSError:
+        err("LOG", f"Could not open '{path}' for saving logs")
+        LOG_FILE = None
+        return False
     
 
 def log(id: str, *kargs):
@@ -19,25 +40,52 @@ def log(id: str, *kargs):
     Log info to stdout.
     """
     
+    global LOG_FILE
+
     for arg in kargs:
         time = datetime.datetime.now()
         sys.stdout.write(f"[{Color.BLUE}{Color.BOLD}INFO{Color.NORMAL}] [{time}] [{Color.BOLD}{id}{Color.NORMAL}]: {arg}\n")
+
+        if LOG_FILE is None:
+            continue
+
+        LOG_FILE.write(f"[INFO] [{time}] [{id}]: {arg}\n")
+        LOG_FILE.flush()
+        
 
 def warn(id: str, *kargs):
     """
     Log a warning to stdout.
     """
     
+    global LOG_FILE
+    
     for arg in kargs:
         time = datetime.datetime.now()
         sys.stdout.write(f"[{Color.YELLOW}{Color.BOLD}WARN{Color.NORMAL}] [{time}] [{Color.BOLD}{id}{Color.NORMAL}]: {arg}\n")
+
+        if LOG_FILE is None:
+            continue
+
+        LOG_FILE.write(f"[WARN] [{time}] [{id}]: {arg}\n")
+        LOG_FILE.flush()
+        
 
 def err(id: str, *kargs):
     """
     Log an error to stderr.
     """
     
+    global LOG_FILE
+
     for arg in kargs:
         time = datetime.datetime.now()
         sys.stderr.write(f"[{Color.RED}{Color.BOLD}ERR{Color.NORMAL}]  [{time}] [{Color.BOLD}{id}{Color.NORMAL}]: {arg}\n")
+
+        if LOG_FILE is None:
+            continue
+
+        LOG_FILE.write(f"[ERR]  [{time}] [{id}]: {arg}\n")
+        LOG_FILE.flush()
+        
 
